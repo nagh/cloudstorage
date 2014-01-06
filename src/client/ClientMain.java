7da99326a7ec309681b8d1da88a1479f7fad31a2
ich731 <schmidt_hh@hotmail.de>
2014-01-06 10:47:13 +0100

    ver 0.27 Christian

--- src/client/ClientMain.java
+++ src/client/ClientMain.java
@@ -20,12 +20,13 @@ import edu.kit.aifb.dbe.hermes.*;
 // Konvention bei Put Requests: List<Serializable>: 1. Eintrag key, 2. Eintrag data
 
 public class ClientMain {
-	
+	// Configuration
 	private static int timeout = 1000; // Zeit [ms]
 	private static int port = 5;       // Port-#
 	private static String ipMaster = "127.0.0.1";
 	private static String ipSlave1 = "127.0.0.1";
 	private static String ipSlave2 = "127.0.0.1";
+	// Initiatlization
 	private static Sender senderM = null;
 	private static Sender senderS1 = null;
 	private static Sender senderS2 = null;
@@ -71,7 +72,13 @@ public class ClientMain {
 		Request request = new Request(payload, "putMasterHandler", null);
 		
 		// Request an den Master senden
-		senderM.sendMessage(request, timeout);
+		Response responseM = senderM.sendMessage(request, timeout);
+		if (responseM.responseCode()) {
+			System.out.println("Der Put-Request war erfolgreich.");
+		}
+		else {
+			System.out.println("Der Put-Request ist fehlgeschlagen.");
+		}
 	}
 	
 	public static void getRequest(String key) {
@@ -80,17 +87,22 @@ public class ClientMain {
 		payload.add(key);
 		Request request = new Request(payload, "getHandler", null);
 		
-		// Requests an den Master und die beiden Slaves senden
-		senderM.sendMessage(request, timeout);
-		senderS1.sendMessage(request, timeout);
-		senderS2.sendMessage(request, timeout);
+		// Request an den Master und die beiden Slaves senden
+		Response responseM = senderM.sendMessage(request, timeout);
+		Response responseS1 = senderS1.sendMessage(request, timeout);
+		Response responseS2 = senderS2.sendMessage(request, timeout);
 		
-		// Receiver auswerten
-		// StackTraceElement[] stackTrace = receiver.getStackTrace();
-		
-		// vergebliche Versuche
-		receiver$NetworkIOHandler.Request;
-		NetworkIOHandler$receiver receiver = new NetworkIOHandler$receiver();
-		NetworkIOHandler networkIOHAndler = new NetworkIOHandler();
+		if (responseM.responseCode() && responseS1.responseCode() && responseS2.responseCode()) {  // check if get-operation was successful
+			if (responseM.getItems().get(2).equals(responseS1.getItems().get(2)) && responseS1.getItems().get(2).equals(responseS2.getItems().get(2))) {  // check if replicas are in consistent state
+				Access.put((String) responseM.getItems().get(1), (String) responseM.getItems().get(2));
+				System.out.println("Der Get-Request war erfolgreich. Das Datum wurde lokal gespeichert.");
+			}
+			else {
+				System.out.println("Die Replika sind nicht konsistent.");
+			}
+		}
+		else {
+			System.out.println("Der Get-Request ist fehlgeschlagen.");
+		}	
 	}
 }
