8c14f0744f5fb8e38d1845264fee3c56e60ae62f
nagh <atyankov@gmail.com>
2014-01-06 11:22:14 +0100

    ver 0.271 Atanas: ServerMain.java altered

--- src/serv/ServerMain.java
+++ src/serv/ServerMain.java
@@ -11,8 +11,20 @@ public class ServerMain {
 	private static int port = 5;
 	private static boolean isMaster = true;
 	private static boolean hasSlave = true; // nur relevant, falls isMaster = false
+	private static boolean sync = true;
 	
-	public static void main(String[] args) {
+	public static void main(String args[]) {
+		
+			System.out.println("Aufruf: ServerMain <master/slave> <slave/noslave> <sync/async>");
+		if (args != null) {
+			isMaster = args[0].equals("true");
+			hasSlave = args[1].equals("true");
+			sync = args[2].equals("true");
+		}
+		System.out.println("Server started with following configuration:");
+		System.out.println("Server is Master: " + isMaster);
+		System.out.println("Server has Slave: " + hasSlave);
+		System.out.println("Synchronous: " + sync);
 		// Basic Setup
 		PropertyConfigurator.configure("log4j.properties");
 		SimpleFileLogger.getInstance();
@@ -21,7 +33,7 @@ public class ServerMain {
 		// Generate and register Handlers
 		reg.registerHandler("getHandler", new GetHandler());
 		if (isMaster == true) {
-			reg.registerHandler("putMasterHandler", new PutMasterHandler());  // PutMasterHandler.java muss noch implementiert werden
+			reg.registerHandler("putMasterHandler", new PutMasterHandler(sync));  // PutMasterHandler.java mit sync-Variable
 		}
 		else if(hasSlave == true) {
 			reg.registerHandler("putSlaveHandler", new PutSlaveHandler(hasSlave));
